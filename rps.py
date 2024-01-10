d35b2a46102bfe0deb431903dcb1dfa67458d3a7
Aleksandrs Ļinovs <you@example.com>
2024-01-10 10:17:46 +0200

    rock

--- rps.py
+++ rps.py
@@ -11,4 +11,27 @@ SCISSOR = "2"
 PAPER = "3"
 
 print("Welcome to Rock, Paper, Scissors!")
+username = input("Username:")
+print("Username is: " + username)
 
+
+Move = input("Your choise:")
+print("You chose: " + Move)
+import random
+result=str(random.randint(1, 3))
+print("computer chose" + result)
+
+if Move == result:
+    print("draw")
+elif Move == ROCK and result == SCISSOR:
+    print("you won")
+elif Move == SCISSOR and result == PAPER:
+    print("you won")   
+elif Move == PAPER and result == ROCK:
+    print("you won")
+elif Move == ROCK and result == PAPER:
+    print("you lost") 
+elif Move == PAPER and result == SCISSOR:
+    print("you lost")   
+elif Move == SCISSOR and result == ROCK:
+    print("you lost")                          
